87c3ed7421337463c7bf52190492f8d803839e92
Milos Jankovic <milos.jankovic@wikala.com>
2019-11-15 15:39:32 +0100

    refactoring

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ import operator
 
 from flask import Flask, jsonify, render_template, request
 
-from .scrappers import tahlia_scrapper as ts
-from .scrappers import weltbild_scrapper as ws
+from .scrappers import ThaliaScrapper, WeltbildScrapper
 
 app = Flask(__name__)
 app.config.from_object('bookworm.config.DevelopmentConfig')
@@ -14,7 +13,10 @@ def index():
     if 'q' in request.args:
         q = request.args.get('q')
 
-        book_list = ts(q) + ws(q)
+        ts = ThaliaScrapper(q)
+        ws = WeltbildScrapper(q)
+
+        book_list = ts.scrap_the_data() + ws.scrap_the_data()
 
         book_list.sort(key=operator.itemgetter('price'))
 
--- scrappers.py
+++ scrappers.py
@@ -14,73 +14,98 @@ def price_format(price, for_display=False):
     return float(first_element)
 
 
-def tahlia_scrapper(q):
-    """
-    scraps the books from thalia.at 
-    """
-    url = 'https://www.thalia.at/suche?filterPATHROOT=c2&sq={}&p=2&pagesize=40'.format(
-        q)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "lxml")
-    books = soup.find_all('div', class_="product")
+class SiteScrapper:
 
-    book_list = []
-    for book in books:
-        title = book.find('span', {'class': 'oProductTitle'}).text
-        price = book.find('span', {'class': 'oPrice'}).text
-        image = book.find('img').get('src')
-        authors = book.find_all('span', class_='oAuthor')
-        link = book.find('a', attrs={'data-info': 'product'}).get('href')
+    def __init__(self, url, tag, clss, format='lxml'):
+        print(url,tag,clss,format)
+        self.url = url
+        self.tag = tag
+        self.clss = clss
+        self.format = format
+        
+        request = requests.get(self.url)
+        soup = BeautifulSoup(request.content, self.format)
+        
+        self.results = soup.find_all(self.tag, class_=self.clss)
 
-        author_list = []
-        for author in authors:
-            if author.text not in author_list:
-                author_list.append(author.text)
-
-        book_dict = {
-            'title': title,
-            'price': price_format(price.strip()),
-            'display_price': price_format(price.strip(), True),
-            'image': image,
-            'by': author_list,
-            'link': 'https://www.thalia.at{}'.format(link)
-        }
-
-        book_list.append(book_dict)
-    return book_list
+    def scrap_the_data(self):
+        raise NotImplementedError("Please Implement this method")        
 
 
-def weltbild_scrapper(q):
-    """
-    scraps the books from weltbild.at 
-    """
-    url = 'https://www.weltbild.at/suche/{}?anzahl=2&node=/buecher'.format(q)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "lxml")
-    book_list = []
-    books = soup.find_all('article', class_="listitem clearfix")
+class ThaliaScrapper(SiteScrapper):
 
-    for book in books:
-        title = book.find('div', {'class': 'title'}).text
-        price = book.find('div', {'class': 'price'}).text
-        image = book.find('div', {'class': 'mediabox'}
-                          ).find('img').get('data-src')
-        authors = book.find_all('div', class_='author')
-        link = book.find('a').get('href')
+    CLSS, TAG = 'product', 'div'
 
-        author_list = []
-        for author in authors:
-            if author.text not in author_list:
-                author_list.append(author.text)
+    def __init__(self, query):
+        url = self.generate_url(query)
+        super().__init__(url, self.TAG, self.CLSS)
 
-        book_dict = {
-            'title': title,
-            'price': price_format(price),
-            'display_price': price_format(price, True),
-            'image': image,
-            'by': author_list,
-            'link': 'https://www.weltbild.at{}'.format(link)
-        }
+    def generate_url(self, query):
+        return f"https://www.thalia.at/suche?filterPATHROOT=c2&sq={query}&p=2&pagesize=40"
 
-        book_list.append(book_dict)
-    return book_list
+    def scrap_the_data(self):
+        book_list = []
+
+        for book in self.results:
+            title = book.find('span', {'class': 'oProductTitle'}).text
+            price = book.find('span', {'class': 'oPrice'}).text
+            image = book.find('img').get('src')
+            authors = book.find_all('span', class_='oAuthor')
+            link = book.find('a', attrs={'data-info': 'product'}).get('href')
+
+            author_list = []
+            for author in authors:
+                if author.text not in author_list:
+                    author_list.append(author.text)
+
+            book_dict = {
+                'title': title,
+                'price': price_format(price.strip()),
+                'display_price': price_format(price.strip(), True),
+                'image': image,
+                'by': author_list,
+                'link': 'https://www.thalia.at{}'.format(link)
+            }
+
+            book_list.append(book_dict)
+        return book_list
+
+
+class WeltbildScrapper(SiteScrapper):
+
+    CLSS, TAG = 'listitem clearfix', 'article'
+
+    def __init__(self, query):
+        url = self.generate_url(query)
+        super().__init__(url, self.TAG, self.CLSS)
+
+    def generate_url(self, query):
+        return f"https://www.weltbild.at/suche/{query}?anzahl=2&node=/buecher"
+
+    def scrap_the_data(self):
+        book_list = []
+
+        for book in self.results:
+            title = book.find('div', {'class': 'title'}).text
+            price = book.find('div', {'class': 'price'}).text
+            image = book.find('div', {'class': 'mediabox'}).find('img').get('data-src')
+            authors = book.find_all('div', class_='author')
+            link = book.find('a').get('href')
+
+            author_list = []
+            for author in authors:
+                if author.text not in author_list:
+                    author_list.append(author.text)
+
+            book_dict = {
+                'title': title,
+                'price': price_format(price),
+                'display_price': price_format(price, True),
+                'image': image,
+                'by': author_list,
+                'link': 'https://www.weltbild.at{}'.format(link)
+            }
+
+            book_list.append(book_dict)
+        return book_list
+    
