dd541df59933ed3e585cd7c6371697eedd47d001
Milos Jankovic <milos.jankovic@wikala.com>
2019-11-15 16:15:07 +0100

    refactoring

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+import operator
+
+from flask import Flask, jsonify, render_template, request
+
+from scrappers import ThaliaScrapper, WeltbildScrapper
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    if 'q' in request.args:
+        q = request.args.get('q')
+
+        ts = ThaliaScrapper(q)
+        ws = WeltbildScrapper(q)
+
+        book_list = ts.scrap_the_data() + ws.scrap_the_data()
+
+        book_list.sort(key=operator.itemgetter('price'))
+
+        return render_template('books.html', books=book_list)
+
+    return render_template('index.html')
